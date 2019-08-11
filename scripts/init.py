d46f0e88e1c6a412b5c4b76a65bcff8fbe83b576
Jacob Roginsky <cyberj19@gmail.com>
2019-08-11 18:40:50 +0300

    add option in init.py to select env_debug or env_public for web service replace run.bat with two run scripts

--- scripts/init.py
+++ scripts/init.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import sys
+import shutil
 
 os.chdir('..')
 root_dir = os.getcwd()
@@ -24,7 +26,15 @@ for d in dirlist:
 	if not os.path.exists(dir):
 		print 'creating dir: %s' % dir
 		os.mkdir(dir)
-		
+
+# prepare env file for web.py
+if len(sys.argv) == 1:
+	env_file = 'public'
+else:
+	env_file = sys.argv[1]
+
+shutil.copyfile('app/web/env_%s.py' % env_file, 'app/web/env.py')
+
 services = [
 	['MONGO', 'C:\\Program Files\\MongoDB\\Server\\3.6\\bin', 'mongod.exe', '--dbpath', os.path.join(os.getcwd(),'data/mongo')],
 	['MOSQUITTO', 'C:\\Program Files\\mosquitto','mosquitto.exe'],
